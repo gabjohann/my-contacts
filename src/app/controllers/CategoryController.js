5d9931e6db6cef4f437144e6260b9604bcfc8e34
Lucas Gabriel <gabif.johann@gmail.com>
2024-12-21 20:22:32 -0300

    add: create a category route

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -18,6 +18,26 @@ class CategoryController {
 
     response.json(category)
   }
+
+  async store(request, response) {
+    const { name } = request.body
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' })
+    }
+
+    const categoryExists = await CategoriesRepository.findByName(name)
+
+    if (categoryExists) {
+      return response
+        .status(400)
+        .json({ error: 'This category already exists' })
+    }
+
+    const category = await CategoriesRepository.create({ name })
+
+    response.json(category)
+  }
 }
 
 module.exports = new CategoryController()
--- src/app/repositories/CategoriesRepository.js
+++ src/app/repositories/CategoriesRepository.js
@@ -20,6 +20,15 @@ class CategoriesRepository {
 
     return row
   }
+
+  async create({ name }) {
+    const [row] = await db.query(
+      'INSERT INTO categories(name) VALUES($1) RETURNING *',
+      [name]
+    )
+
+    return row
+  }
 }
 
 module.exports = new CategoriesRepository()
