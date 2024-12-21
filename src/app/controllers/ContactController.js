20b44fceaa1e5cd44811d4881b604d2443b0f962
Lucas Gabriel <gabif.johann@gmail.com>
2024-12-21 11:04:07 -0300

    add: edit contact route

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -33,7 +33,7 @@ class ContactController {
     if (contactExists) {
       return response
         .status(400)
-        .json({ error: 'This e-mail is already been taken' })
+        .json({ error: 'This e-mail is already in use' })
     }
 
     const contact = await ContactsRepository.create({
@@ -47,7 +47,37 @@ class ContactController {
   }
 
   //Editar um registro
-  update() {}
+  async update(request, response) {
+    const { id } = request.params
+    const { name, email, phone, category_id } = request.body
+
+    const contactExists = await ContactsRepository.findById(id)
+
+    if (!contactExists) {
+      return response.status(404).json({ error: 'User not found' })
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' })
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email)
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response
+        .status(400)
+        .json({ error: 'This e-mail is already in use' })
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name,
+      email,
+      phone,
+      category_id,
+    })
+
+    response.json(contact)
+  }
 
   // Deletar um registro
   async delete(request, response) {
