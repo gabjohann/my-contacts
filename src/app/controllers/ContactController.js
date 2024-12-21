8c4d1a5d3c70cead339ff6fe0415f29ccff5248a
Lucas Gabriel <gabif.johann@gmail.com>
2024-12-21 10:02:56 -0300

    add: delete contact route

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -27,7 +27,19 @@ class ContactController {
   update() {}
 
   // Deletar um registro
-  delete() {}
+  async delete(request, response) {
+    const { id } = request.params
+
+    const contact = await ContactsRepository.findById(id)
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' })
+    }
+
+    await ContactsRepository.delete(id)
+
+    response.sendStatus(204)
+  }
 }
 
 module.exports = new ContactController()
