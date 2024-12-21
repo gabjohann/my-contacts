d905ad5b823fef9fdf158d25d1933a5a7e676a50
Lucas Gabriel <gabif.johann@gmail.com>
2024-12-21 10:38:19 -0300

    add: create user route

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -21,7 +21,30 @@ class ContactController {
   }
 
   // Criar um novo registro
-  store() {}
+  async store(request, response) {
+    const { name, email, phone, category_id } = request.body
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' })
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email)
+
+    if (contactExists) {
+      return response
+        .status(400)
+        .json({ error: 'This e-mail is already been taken' })
+    }
+
+    const contact = await ContactsRepository.create({
+      name,
+      email,
+      phone,
+      category_id,
+    })
+
+    response.json(contact)
+  }
 
   //Editar um registro
   update() {}
